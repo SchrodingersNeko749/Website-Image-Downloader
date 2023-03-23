b9df3a1847f4cc95c9c7a8ca788a0d388be8ee92
SchrodingersNeko749 <44234331+SchrodingersNeko749@users.noreply.github.com>
2023-03-23 18:36:02 +0330

    Create wid.py

---
+++ wid.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+import os 
+import argparse
+import requests
+from bs4 import BeautifulSoup
+import re
+
+def print_options():
+    print("usage: imgd -p|--path <path> -u|--url <url>")
+
+
+# grab the input  
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-u", "--url", help="the url of the images you want to download")
+parser.add_argument("-p","--path", help="the download path")
+
+# # parse the arguments
+args = parser.parse_args()
+
+path_name = args.path
+url = args.url
+if path_name and url:
+    try:
+        # make get request
+         response = requests.get(url)
+         # parse html and get image tags
+         if response:
+             soup = BeautifulSoup(response.text, "html.parser")
+             img_tags = soup.find_all("img")
+
+             #make folder and download all images to its images 
+             try:
+                 os.mkdir(path_name)
+             except FileExistsError:
+                 print(path_name,"already exists. skipping folder creation")
+             image_counter = 0 # for file naming
+             for img in img_tags:
+                src = img.get("src")
+                if src and re.search(r"\.(jpg|jpeg|png)$", src):
+                    filename = re.search(r"/([\w\.-]+)$", src)
+                    print(filename.group(1))
+
+                    response = requests.get(src)
+
+                    with open(path_name+filename.group(1), "wb") as f:
+                        f.write(response.content)
+                    
+
+    except TimeoutError:
+        print("didnt get any requests. check if url is correct")
+
+else:
+    print("specify a url and a path")
+    print_options()
+
